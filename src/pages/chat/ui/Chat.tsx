6356da706efdccf9ca09a71b91a726e1c5272c0a
YXUNGGG <s.krivostanenko@mail.ru>
2025-05-18 19:09:49 +0500

    ggg

--- src/pages/chat/ui/Chat.tsx
+++ src/pages/chat/ui/Chat.tsx
@@ -44,7 +44,7 @@ const Chat: React.FC<OwnProps> = ({chatid}) => {
     <div className="layout">
       <div className="layout-block Chat">
           <div id="messages">
-          {messages && messages.map((message, id) => <Message
+          {!messages.length === 0 && messages.map((message, id) => <Message
             content={message.content ? message.content : "hidden"}
             isOwn={message.sender === userData?.name}
             sender={messages[id + 1] && messages[id + 1].sender === message.sender
